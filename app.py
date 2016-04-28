9d7390480b6e23ea6e2969df0085f9c687b3a3f5
zmrowicki@hotmail.com <zmrowicki@hotmail.com>
2016-04-27 22:15:17 -0700

    Add a few dynamic routes

--- app.py
+++ app.py
@@ -1,12 +1,40 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
 @app.route('/')
 @app.route('/hello')
 
 def hello_world():
     return 'Hello World'
+
+@app.route("/test/<search_query>")
+def search(search_query):
+    return search_query
+
+@app.route("/integer/<int:value>")
+def int_type(value):
+    print value + 1
+    return "correct"
+
+@app.route("/float/<float:value>")
+def float(value):
+    print value + 1
+    return "correct"
+
+@app.route("/path/<path:value>")
+def path_type(value):
+    print value
+    return "correct"
+
+@app.route("/name/<name>")
+def index(name):
+    if name.lower() == "michael":
+        return "hello, {}".format(name), 200
+    else:
+        return "Not found", 404
+
 
 if __name__ == '__main__':
     app.run()
