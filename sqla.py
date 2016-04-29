0f23516027725f0114aabc7b406ee8df06cad07f
zmrowicki@hotmail.com <zmrowicki@hotmail.com>
2016-04-29 16:38:15 -0700

    Use a with statement for more compact code

--- sqla.py
+++ sqla.py
@@ -1,13 +1,6 @@
 import sqlite3
 
-conn = sqlite3.connect("new.db")
-
-cursor = conn.cursor()
-
-#cursor.execute("""CREATE TABLE inventory(Make TEXT, Model TEXT, Quantity INT)""")
-cursor.execute("""INSERT INTO population VALUES('New York City', 'NY', 8200000)""")
-cursor.execute("""INSERT INTO population VALUES('San Francisco', 'CA', 800000)""")
-
-conn.commit()
-
-conn.close()
+with sqlite3.connect('new.db') as connection:
+    c = connection.cursor()
+    cursor.execute("""INSERT INTO population VALUES('New York City', 'NY', 8200000)""")
+    cursor.execute("""INSERT INTO population VALUES('San Francisco', 'CA', 800000)""")
